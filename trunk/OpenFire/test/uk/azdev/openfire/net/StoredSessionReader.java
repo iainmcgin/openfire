b0ac076987356509d9d6f930a94db8bdc98dc674
iainmcgin <iainmcgin@eb094506-3f28-0410-a470-1fd92c4471f4>
2007-06-01 23:01:15 +0000

    session reader can now dump read messages to files convenient for generating test cases

--- trunk/OpenFire/test/uk/azdev/openfire/net/StoredSessionReader.java
+++ trunk/OpenFire/test/uk/azdev/openfire/net/StoredSessionReader.java
@@ -20,11 +20,10 @@ package uk.azdev.openfire.net;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
 
 import uk.azdev.openfire.net.messages.IMessage;
@@ -52,9 +51,11 @@ public class StoredSessionReader {
 			return;
 		}
 		
+		File messageOutputRoot = getMessageOutputRoot(args);
+		
 		ReadableByteChannel channel = getChannel(fileToRead, numBytesToStrip);
 		ChannelReader reader = new ChannelReader(channel);
-		readAllMessages(reader);
+		readAllMessages(reader, messageOutputRoot);
 	}
 	
 	
@@ -84,7 +85,7 @@ public class StoredSessionReader {
 				return -1;
 			}
 			
-			if(numBytesToStrip <= 0) {
+			if(numBytesToStrip < 0) {
 				System.err.println("Second argument is not a positive integer");
 				return -1;
 			}
@@ -93,19 +94,40 @@ public class StoredSessionReader {
 		return numBytesToStrip;
 	}
 	
-	private static ReadableByteChannel getChannel(File fileToRead, int numBytesToStrip) throws IOException {
-		InputStream messageStream = getResource(fileToRead);
-		ReadableByteChannel channel = Channels.newChannel(messageStream);
+	private static File getMessageOutputRoot(String[] args) {
+		if(args.length < 3) {
+			return null;
+		}
+		
+		String dirPath = args[2];
+		File outputRoot = new File(dirPath);
+		if(!outputRoot.exists()) {
+			System.err.println("Message output root directory \"" + dirPath + "\" does not exist");
+			return null;
+		}
+		
+		if(!outputRoot.isDirectory()) {
+			System.err.println("Message output root \"" + outputRoot + "\" is not a directory");
+			return null;
+		}
+		
+		return outputRoot;
+	}
+	
+	private static FileChannel getChannel(File fileToRead, int numBytesToStrip) throws IOException {
+		FileInputStream messageStream = new FileInputStream(fileToRead);
+		FileChannel channel = messageStream.getChannel();
+		
 		stripBytes(channel, numBytesToStrip);
 		
 		return channel;
 	}
 	
-	private static InputStream getResource(File file) throws FileNotFoundException {
-		return new FileInputStream(file);
-	}
-	
 	private static void stripBytes(ReadableByteChannel channel, int numBytesToStrip) throws IOException {
+		if(numBytesToStrip == 0) {
+			return;
+		}
+		
 		ByteBuffer buffer = IOUtil.createBuffer(numBytesToStrip);
 		int numRead = channel.read(buffer);
 		if(numRead != numBytesToStrip) {
@@ -113,18 +135,42 @@ public class StoredSessionReader {
 		}
 	}
 	
-	private static void readAllMessages(ChannelReader reader) {
+	private static void readAllMessages(ChannelReader reader, File messageOutputRoot) {
 		IMessage message;
 		try {
 			while((message = reader.readMessage()) != null) {
 				System.out.println(message);
 				System.out.println();
+				
+				writeMessageData(message, messageOutputRoot);
 			}
+			System.out.println("Done.");
 		} catch (IOException e) {
 			System.err.println("Error occurred while attempting to read message");
 			e.printStackTrace();
 		}
 		
+	}
+
+
+
+	private static void writeMessageData(IMessage message, File messageOutputRoot) throws IOException {
+		if(messageOutputRoot == null) {
+			return;
+		}
+		
+		int id = message.getMessageId();
+		
+		File tempFile = File.createTempFile(String.format("%1$07d_", id), ".dat", messageOutputRoot);
+		
+		System.out.println("Writing data to \"" + tempFile.getPath() + "\"");
+		
+		FileChannel channel = new FileOutputStream(tempFile).getChannel();
+		ByteBuffer buffer = IOUtil.createBuffer(message.getMessageContentSize());
+		message.writeMessageContent(buffer);
+		buffer.flip();
+		channel.write(buffer);
+		channel.close();
 	}
 	
 }
